879b9905de1a3f13961c60d8ab03b4826e506edf
rebortg <github@ghlr.de>
2022-05-29 20:57:34 +0200

    improve leafnode detection and outputfiles, add json output with metadata from vyos

--- cmd_extractor.py
+++ cmd_extractor.py
@@ -1,4 +1,5 @@
 from paramiko import SFTPClient, Transport
+import datetime
 from collections import OrderedDict
 from stat import S_ISDIR, S_ISREG
 import json
@@ -19,6 +20,7 @@ oppath = '/opt/vyatta/share/vyatta-op/templates'
 
 
 def main():
+    now = datetime.datetime.now()
 
     parser = argparse.ArgumentParser(description='VyOS Command extractor')
     parser.add_argument('-s','--host', help='IP/Hostname of a VyOS Host', required=True)
@@ -36,29 +38,51 @@ def main():
     t = Transport((vyos_ip, 22))
     t.connect(username=vyos_user, password=vyos_password)
     f = SFTPClient.from_transport(t)
+    # get vyos meta data
+    logger.info(f'get /etc/os-release')
+    with f.open('/etc/os-release', "r") as osfile:
+        for line in osfile.readlines():
+            if "VERSION_ID" in line:
+                os_version = line.split('="')[1].strip()[:-1]
+
+
     logger.info(f'listdir: {cfgcmd}')
     files = listdir(f, cfgpath)
     for file in files:
         logger.info(f'parse: {file}')
-        cfgcmd.append(parse_file(f, file))
+        cmd = parse_file(f, file)
+        if cmd:
+            cfgcmd.append(cmd)
     
     logger.info(f'listdir: {oppath}')
     files = listdir(f, oppath)
     for file in files:
         logger.info(f'parse: {file}')
-        opcmd.append(parse_file(f, file))
+        cmd = parse_file(f, file)
+        if cmd:
+            opcmd.append(cmd)
     
-    logger.info(f'write output/cfgcmd.txt')
-    f = open('output/cfgcmd.txt', 'w')
+    logger.info(f"output/{now.strftime('%Y%m%d')}-{os_version}_cfg.txt")
+    f = open(f"output/{now.strftime('%Y%m%d')}-{os_version}_cfg.txt", 'w')
     for cmd in cfgcmd:
         render_cmd(cmd, f)    
     f.close()
 
-    logger.info(f'write output/opcmd.txt')
-    f = open('output/opcmd.txt', 'w')
+    logger.info(f"output/{now.strftime('%Y%m%d')}-{os_version}_op.txt")
+    f = open(f"output/{now.strftime('%Y%m%d')}-{os_version}_op.txt", 'w')
     for cmd in opcmd:
-        render_cmd(cmd, f)    
+        render_cmd(cmd, f)
     f.close()
+
+    output = {
+        "os": os_version,
+        "date": datetime.datetime.isoformat(now),
+        "cfgcmd": cfgcmd,
+        "opcmd": opcmd,
+    }
+
+    with open(f"output/{now.strftime('%Y%m%d')}-{os_version}.json", "w") as outfile:
+        json.dump(output, outfile)
 
 
 def listdir(sftp, remotedir, ignore_dir=""):
@@ -70,7 +94,6 @@ def listdir(sftp, remotedir, ignore_dir=""):
             absolutefilenames.extend(listdir(sftp, absolutepath))
         if S_ISREG(entry.st_mode):
             logger.info(f'got file: {absolutepath}')
-
             absolutefilenames.append(absolutepath)
     return absolutefilenames
 
@@ -93,27 +116,44 @@ def parse_file(sftp, filepath):
 
     with sftp.open(filepath, "r") as f:
         # get command from path
+        if "vyatta-op" not in filepath and "node.def" == filepath[-8:]:
+            files = listdir(sftp, filepath[:-9])
+            if len(files) > 1:
+                return None
+        
         filepath = filepath.replace('node.tag', '<text>')
         filepath = filepath.replace('node.def', '<value>')
+
         cmd = filepath.split("/")[6:]
         for line in f.readlines():
             if "help: " in line:
                 help = line[6:].rstrip()
-            if "run:" in line:
+            elif "run:" in line:
                 run = line[5:].rstrip()
-            if "type:" in line:
+            elif "type:" in line:
                 type = line[6:].rstrip()
-            if "val_help:" in line:
+            elif "val_help:" in line:
                 val_help = line[10:].rstrip()
-        
-        # without type the command is a leaf node
-        if not type:
-            cmd = cmd[:-1]
-    
+            elif "allowed" in line:
+                pass
+            elif "syntax" in line:
+                pass
+            elif "multi" in line:
+                pass
+            elif "tag" in line:
+                pass
+
+    # op cmd if run in node.def == leaf node
+    if oppath in filepath:
+        if run is None:
+            return None
+        if cmd[-1] == "<value>":
+            cmd.pop(-1)
+    if cfgpath in filepath:
+        if type is None and cmd[-1] == "<value>":
+            cmd.pop(-1)
+
     return {"cmd": cmd, "run": run, "help": help, "type": type, "val_help": val_help}
-
-
-
 
 
 if __name__ == "__main__":
